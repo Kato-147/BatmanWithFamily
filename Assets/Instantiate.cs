a4b1d09d9aa1097b312b80c3ff424f5f63c79f9b
phaanh <102342724+phaanh@users.noreply.github.com>
2023-11-09 15:01:48 +0700

    video1

---
+++ Assets/Instantiate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+internal class Instantiate
+{
+    private GameObject gameObject;
+    private Vector3 vector3;
+    private Quaternion identity;
+
+    public Instantiate(GameObject gameObject, Vector3 vector3, Quaternion identity)
+    {
+        this.gameObject = gameObject;
+        this.vector3 = vector3;
+        this.identity = identity;
+    }
+}
