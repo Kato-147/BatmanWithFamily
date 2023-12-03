f2200415c03de897ec057dfd05921da778dad2fb
Tỏn nè <oroki147@gmail.com>
2023-12-03 19:25:34 +0700

    2

--- Assets/SpawnNodes.cs
+++ Assets/SpawnNodes.cs
@@ -11,17 +11,17 @@ public class SpawnNodes : MonoBehaviour
 
     void Start()
     {
-        gameObject.name = "Node";
-        return;
-        if (gameObject.name =="Node")
+       /* gameObject.name = "Node";
+        return;*/
+        if (gameObject.name == "Node") { 
             currentSpawnOffset = spawnOffset;
             for (int i = 0; i < numToSpawn; i++)
                 {
                     //clone a new node
-                    GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y+ currentSpawnOffset, 0), Quaternion.identity);
+                    GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y + currentSpawnOffset, 0), Quaternion.identity);
             currentSpawnOffset += spawnOffset;
                 }
-            {
+            
 
         }
         
