03c41e91898361df1a29439d47c21949625db538
locit123 <phungloc6102003@gmail.com>
2023-12-05 12:23:23 +0700

    Xong Api Alll

---
+++ Assets/ScriptsApi/ResponseRegister.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponseRegister 
+{
+    public ResponseRegister(int status, string notification)
+    {
+        this.status = status;
+        this.notification = notification;
+    }
+
+    /*
+* "status": 0
+* "notification":"đăng nhập ko thành công"
+
+*/
+    public int status { get; set; }
+    public string notification { get; set; }
+
+}
+
+
