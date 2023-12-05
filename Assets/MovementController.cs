f023e2512f5f5400dd96e303566d03df8c50ce28
Tỏn nè <oroki147@gmail.com>
2023-12-05 07:42:45 +0700

    21 lỗi quá trời lỗi

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -10,49 +10,55 @@ public class MovementController : MonoBehaviour
     public float speed = 4f;
 
     public string direction = "";
-    public string lastMovingDirection = "";
+    public string lastMovingDirrection = "";
 
-    public bool canWarp = true;
+    public bool canWarp;
 
     public bool isGhost = false;
 
-
-    // Start is called before the first frame update
     void Awake()
     {
+
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (!gameManager.gameIsRunning)
+        {
+            return;
+        }
         NodeController currentNodeController = currentNode.GetComponent<NodeController>();
-
         transform.position = Vector2.MoveTowards(transform.position, currentNode.transform.position, speed * Time.deltaTime);
 
         bool reverseDirection = false;
-        if ((direction == "left" && lastMovingDirection == "right")
-            || (direction == "right" && lastMovingDirection == "left")
-            || (direction == "up" && lastMovingDirection == "down")
-            || (direction == "down" && lastMovingDirection == "up")
+
+        if (
+            (direction == "left" && lastMovingDirrection == "right")
+            || (direction == "right" && lastMovingDirrection == "left")
+            || (direction == "up" && lastMovingDirrection == "down")
+            || (direction == "down" && lastMovingDirrection == "up")
             )
         {
             reverseDirection = true;
         }
 
-        //Figure out if we're at the center of our current node
+        // Figure out if we're at the center of our current node
         if ((transform.position.x == currentNode.transform.position.x && transform.position.y == currentNode.transform.position.y) || reverseDirection)
         {
+
             if (isGhost)
             {
                 GetComponent<EnemyController>().ReachedCenterOfNode(currentNodeController);
             }
-            //If we reached the center of the left warp , warp to the right warp
+            //If we reached the center of the left warp, warp to the right warp
             if (currentNodeController.isWarpLeftNode && canWarp)
             {
                 currentNode = gameManager.rightWarpNode;
                 direction = "left";
-                lastMovingDirection = "left";
+                lastMovingDirrection = "left";
                 transform.position = currentNode.transform.position;
                 canWarp = false;
             }
@@ -61,25 +67,32 @@ public class MovementController : MonoBehaviour
             {
                 currentNode = gameManager.leftWarpNode;
                 direction = "right";
-                lastMovingDirection = "right";
+                lastMovingDirrection = "right";
                 transform.position = currentNode.transform.position;
-                canWarp=false;
+                canWarp = false;
             }
-            //Otherwise, find the next node we are going to be moving towards
+            // otherwise, find the next node
             else
             {
+                // if we are not a ghost that is respawning, and we are not startnode, and we trying to move down, stop
+                if (currentNodeController.isGhostStartingNode && direction == "down"
+                    && (!isGhost || GetComponent<EnemyController>().ghostNodeState != EnemyController.GhostNodeStateEnum.respawning))
+                {
+                    direction = lastMovingDirrection;
+                }
+
                 //Get the next node from our node controller using our current direction
                 GameObject newNode = currentNodeController.GetNodeFromDirection(direction);
-                //If we can move in the desired direction
+                // if we can go to the desired direction
                 if (newNode != null)
                 {
                     currentNode = newNode;
-                    lastMovingDirection = direction;
+                    lastMovingDirrection = direction;
                 }
-                //We can't move in desired direction, try to keep going in the last moving direction
+                //We cant go to the desired direction, keep going in the last moving direction
                 else
                 {
-                    direction = lastMovingDirection;
+                    direction = lastMovingDirrection;
                     newNode = currentNodeController.GetNodeFromDirection(direction);
                     if (newNode != null)
                     {
@@ -88,13 +101,18 @@ public class MovementController : MonoBehaviour
                 }
             }
 
-
         }
-        //We aren't in the center of a node
+        //Not in center of a node
         else
         {
             canWarp = true;
         }
+
+    }
+
+    public void SetSpeed(float newspeed)
+    {
+        speed = newspeed;
     }
 
     public void SetDirection(string newDirection)
--- Assets/NodeController.cs
+++ Assets/NodeController.cs
@@ -1,10 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeController : MonoBehaviour
 {
-
     public bool canMoveLeft = false;
     public bool canMoveRight = false;
     public bool canMoveUp = false;
@@ -18,91 +18,112 @@ public class NodeController : MonoBehaviour
     public bool isWarpRightNode = false;
     public bool isWarpLeftNode = false;
 
-    //If the node contains  a pellet when the game start
+    //If the node contains a pellet when the game starts
     public bool isPelletNode = false;
     //If the node still has a pellet
     public bool hasPellet = false;
 
-    public SpriteRenderer pelletSprite;
+    public bool isGhostStartingNode = false;
+
+    public SpriteRenderer pellet;
+
     public GameManager gameManager;
 
-    // Start is called before the first frame update
+    public bool isSideNode = false;
+
+    public bool isPowerPellet = false;
+
+    public float powerPelletBlinkingTimer = 0;
+
     void Awake()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        pellet = GetComponentInChildren<SpriteRenderer>();
         if (transform.childCount > 0)
         {
-            hasPellet = true;
+            gameManager.GotPelletFromNodeController(this);
             isPelletNode = true;
-            pelletSprite = GetComponentInChildren<SpriteRenderer>();
+            hasPellet = true;
         }
-
-        RaycastHit2D[] hitsDown;
-        //Shoot raycast (line) going down
-        hitsDown = Physics2D.RaycastAll(transform.position, -Vector2.up);
+        // shoot raycast line going down
+        RaycastHit2D[] hitsDown = Physics2D.RaycastAll(transform.position, Vector2.down);
 
         //Loop through all of the gameobjects that the raycast hits
-        for (int i = 0; i < hitsDown.Length; i++)
+        foreach (RaycastHit2D hit in hitsDown)
         {
-            float distance = Mathf.Abs(hitsDown[i].point.y - transform.position.y);
-            if (distance < 0.4f && hitsDown[i].collider.tag == "Node")
+            float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            if (distance < 0.4f && hit.collider.tag == "Node")
             {
                 canMoveDown = true;
-                nodeDown = hitsDown[i].collider.gameObject;
+                nodeDown = hit.collider.gameObject;
             }
         }
 
-        RaycastHit2D[] hitsUp;
-        //Shoot raycast (line) going up
-        hitsUp = Physics2D.RaycastAll(transform.position, Vector2.up);
+        // shoot raycast line going up
+        RaycastHit2D[] hitsUp = Physics2D.RaycastAll(transform.position, Vector2.up);
 
         //Loop through all of the gameobjects that the raycast hits
-        for (int i = 0; i < hitsUp.Length; i++)
+        foreach (RaycastHit2D hit in hitsUp)
         {
-            float distance = Mathf.Abs(hitsUp[i].point.y - transform.position.y);
-            if (distance < 0.4f && hitsUp[i].collider.tag == "Node")
+            float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            if (distance < 0.4f && hit.collider.tag == "Node")
             {
                 canMoveUp = true;
-                nodeUp = hitsUp[i].collider.gameObject;
+                nodeUp = hit.collider.gameObject;
             }
         }
 
-        RaycastHit2D[] hitsRight;
-        //Shoot raycast (line) going right
-        hitsRight = Physics2D.RaycastAll(transform.position, Vector2.right);
+        // shoot raycast line going right
+        RaycastHit2D[] hitsRight = Physics2D.RaycastAll(transform.position, Vector2.right);
 
         //Loop through all of the gameobjects that the raycast hits
-        for (int i = 0; i < hitsRight.Length; i++)
+        foreach (RaycastHit2D hit in hitsRight)
         {
-            float distance = Mathf.Abs(hitsRight[i].point.x - transform.position.x);
-            if (distance < 0.4f && hitsRight[i].collider.tag == "Node")
+            float distance = Mathf.Abs(hit.point.x - transform.position.x);
+            if (distance < 0.4f && hit.collider.tag == "Node")
             {
                 canMoveRight = true;
-                nodeRight = hitsRight[i].collider.gameObject;
+                nodeRight = hit.collider.gameObject;
             }
         }
 
-        RaycastHit2D[] hitsLeft;
-        //Shoot raycast (line) going right
-        hitsLeft = Physics2D.RaycastAll(transform.position, -Vector2.right);
+        // shoot raycast line going left
+        RaycastHit2D[] hitsLeft = Physics2D.RaycastAll(transform.position, Vector2.left);
 
         //Loop through all of the gameobjects that the raycast hits
-        for (int i = 0; i < hitsLeft.Length; i++)
+        foreach (RaycastHit2D hit in hitsLeft)
         {
-            float distance = Mathf.Abs(hitsLeft[i].point.x - transform.position.x);
-            if (distance < 0.4f && hitsLeft[i].collider.tag == "Node")
+            float distance = Mathf.Abs(hit.point.x - transform.position.x);
+            if (distance < 0.4f && hit.collider.tag == "Node")
             {
                 canMoveLeft = true;
-                nodeLeft = hitsLeft[i].collider.gameObject;
+                nodeLeft = hit.collider.gameObject;
             }
         }
 
+        if (isGhostStartingNode)
+        {
+            canMoveDown = true;
+            nodeDown = gameManager.ghostNodeCenter;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (!gameManager.gameIsRunning)
+        {
+            return;
+        }
+        if (isPowerPellet && hasPellet)
+        {
+            powerPelletBlinkingTimer += Time.deltaTime;
+            if (powerPelletBlinkingTimer >= 0.1f)
+            {
+                powerPelletBlinkingTimer = 0;
+                pellet.enabled = !pellet.enabled;
+            }
+        }
     }
 
     public GameObject GetNodeFromDirection(string direction)
@@ -115,17 +136,26 @@ public class NodeController : MonoBehaviour
         {
             return nodeRight;
         }
-        else if (direction == "up" && canMoveUp)
-        {
-            return nodeUp;
-        }
         else if (direction == "down" && canMoveDown)
         {
             return nodeDown;
         }
+        else if (direction == "up" && canMoveUp)
+        {
+            return nodeUp;
+        }
         else
         {
             return null;
+        }
+    }
+
+    public void RespawnPellet()
+    {
+        if (isPelletNode)
+        {
+            hasPellet = true;
+            pellet.enabled = true;
         }
     }
 
@@ -134,8 +164,8 @@ public class NodeController : MonoBehaviour
         if (collision.tag == "Player" && hasPellet)
         {
             hasPellet = false;
-            pelletSprite.enabled = false;
-            gameManager.CollectedPellet(this);
+            pellet.enabled = false;
+            StartCoroutine(gameManager.CollectedPellet(this));
         }
     }
 }
